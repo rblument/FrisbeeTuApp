2f2d98a8db6153e25d40a3befe59859a01952bca
Thomas O'Rourke <torourke@regis.edu>
2024-10-31 13:18:07 -0600

    Create branch for models and semantics code

---
+++ src/main/java/edu/regis/frisbee/view/TomsView.java
@@ -0,0 +1,23 @@
+/*
+ * Frisbee: Formal Logic Tutor
+ * 
+ *  (C) Richard Blumenthal, All rights reserved
+ * 
+ *  Unauthorized use, duplication or distribution without the authors'
+ *  permission is strictly prohibited.
+ * 
+ *  Unless required by applicable law or agreed to in writing, this
+ *  software is distributed on an "AS IS" basis without warranties
+ *  or conditions of any kind, either expressed or implied.
+ */
+package edu.regis.frisbee.view;
+
+/**
+ *
+ * @author Thomas O'Rourke
+ */
+public class TomsView {
+    public void initialize() {
+        System.out.println("Tom's View not implemented");
+    }
+}
