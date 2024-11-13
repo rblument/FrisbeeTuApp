06f2bc840086ddcb94c1a5f9339bca13a427332f
Sophie <sophia.rose0809@gmail.com>
2024-11-13 11:10:55 -0700

    added a title, testing merge with sofias branch

--- src/main/java/edu/regis/frisbee/view/LogicEquivalence.java
+++ src/main/java/edu/regis/frisbee/view/LogicEquivalence.java
@@ -12,75 +12,44 @@
  */
 package edu.regis.frisbee.view;
 
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.BorderFactory;
 import javax.swing.JButton;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
 import javax.swing.JTable;
-import javax.swing.JTextField;
-import javax.swing.border.TitledBorder;
-
 /**
  *
  * @author Sophie Holland
  */
 public class LogicEquivalence extends GPanel implements ActionListener {
     
-    private JTextField fooField;
+    private JLabel title;
     
     private JButton enterBut;
     
     public LogicEquivalence() {
         initializeComponents();
-        layoutComponents();
-        
-        JFrame frame = new JFrame();
-        JPanel panel = new JPanel();
-        panel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "Logical Equivalences", TitledBorder.CENTER, TitledBorder.TOP));
-        
-        String[][] equivalences = {
-            {"Commutative Properties", "p∨q≡q∨p", "p∧q≡q∧p"},
-            {"Associative Properties", "(p∨q)∨r≡p∨(q∨r)", "(p∧q)∧r≡p∧(q∧r)"},
-        };
-        String[] header = {"Property Name", "Property 1", "Property 2"};
-        JTable table = new JTable(equivalences, header);
-        panel.add(new JScrollPane(table));
-        frame.add(panel);
-        frame.setVisible(true);
+        display();
     }
     
-//    public static void view(String[] args) {
-//        JFrame frame = new JFrame();
-//        JPanel panel = new JPanel();
-//        panel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "Logical Equivalences", TitledBorder.CENTER, TitledBorder.TOP));
-//        
-//        String[][] equivalences = {
-//            {"Commutative Properties", "p∨q≡q∨p", "p∧q≡q∧p"},
-//            {"Associative Properties", "(p∨q)∨r≡p∨(q∨r)", "(p∧q)∧r≡p∧(q∧r)"},
-//        };
-//        String[] header = {"Property Name", "Property 1", "Property 2"};
-//        JTable table = new JTable(equivalences, header);
-//        panel.add(new JScrollPane(table));
-//        frame.add(panel);
-//        frame.setVisible(true);
-//    }
-    
     private void initializeComponents() {
-        fooField = new JTextField();
-        fooField.setText("Enter Logic Equivalence");
+        title = new JLabel();
+        title.setText("Practice: Logic Equivalences");
         
         enterBut = new JButton("Enter");
         enterBut.addActionListener(this);
     }
     
-    private void layoutComponents() {
-        
-    }
+    private void display() {   
+        title.setFont(new Font("Serif", Font.PLAIN, 24));
+
+        addc(title, 0, 0, 1, 1, 0.1, 0.1,
+           GridBagConstraints.NORTHWEST, GridBagConstraints.NONE,
+           5, 5, 5, 5);
+
+    }    
     
     @Override
     public void actionPerformed(ActionEvent e) {
